513fdde3c9678c7b42573d30e7a8b6f98bbaab7c
hjj <1689692283@qq.com>
2019-04-11 20:00:06 +0800

    test

---
+++ src/main/java/com/baizhi/controller/userController.java
@@ -0,0 +1,10 @@
+package com.baizhi.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class userController {
+    public String test(){
+        return "aa";
+    }
+}
