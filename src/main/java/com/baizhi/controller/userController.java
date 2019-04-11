2e4713175144da6f35f2a0bc9875be0970209e1b
hjj007 <49516835+hjj007@users.noreply.github.com>
2019-04-11 20:02:58 +0800

    Update userController.java

--- src/main/java/com/baizhi/controller/userController.java
+++ src/main/java/com/baizhi/controller/userController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class userController {
     public String test(){
+        System.out.println("1234567");
         return "aa";
     }
 }
